bfdcadbbbd83bfca08ee5046b9f297b8f97a270a
nikhil chouksey <nikhilranger18@gmail.com>
2025-10-10 22:58:20 +0530

    JWT-setup + user registration/login

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+
 
 dotenv.config();
 console.log("PORT from .env:", process.env.PORT);
@@ -10,6 +12,8 @@ const app = express();
 
 app.use(cors()); 
 app.use(express.json()); 
+app.use("/api/users", userRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to PrepTube");
